c1abde1bedfdc2e3e8d69d0804a6adbe291df8ce
jesse <webbj19@ferris.edu>
2020-02-26 16:04:48 -0500

    Addded controloer key board and mosue to menue
    Added mainmenucontroller
    added rollover

---
+++ Assets/MainMenueController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenueController : MonoBehaviour
+{
+
+
+    EventSystem events;
+    // Start is called before the first frame update
+    void Start()
+    {
+      events =  GetComponentInChildren<EventSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(events.currentSelectedGameObject == null)
+        {
+            if(events.firstSelectedGameObject != null)
+            {
+                events.SetSelectedGameObject(events.firstSelectedGameObject);
+            }
+        }
+    }
+    public void Button1Pressed()
+    {
+        print("button 1 was pressed");
+    }
+    public void Button2Pressed()
+    {
+        print("button 2 was pressed");
+    }
+    public void Button3Pressed()
+    {
+        print("button 3 was pressed");
+    }
+    public void Button4Pressed()
+    {
+        print("button 4 was pressed");
+    }
+    public void Button5Pressed()
+    {
+        print("button 5 was pressed");
+    }
+    public void Button6Pressed()
+    {
+        print("button 6 was pressed");
+    }
+}
