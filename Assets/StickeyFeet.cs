d56f098296dc470c5e4259830c051e0350e0c68e
jesse <webbj19@ferris.edu>
2020-04-15 15:35:50 -0400

    worked on multi leg creatuer
    added spider rig
    added stickey feet
    added spider to test rig

---
+++ Assets/StickeyFeet.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickeyFeet : MonoBehaviour
+{
+    public StickeyFeet[] otherStickeyFeet;
+    public Transform homeLocation;
+    Vector3 plantedPosLast;
+    Vector3 plantedPosNext;
+    public float moveDistanceThreshold = .5f;
+    public float timeToMoveFoot = 0.25f;
+    float footMoveTimer = 0;
+    [Header("footanimation:")]
+    public AnimationCurve footlateralEase;
+    public AnimationCurve footVerticalRaise;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (StickeyFeet )
+        CheckIfCanMoveFoot();
+        AnimatedFoot();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void AnimatedFoot()
+    {
+
+        if (footMoveTimer >= timeToMoveFoot)
+        {
+            
+        
+    transform.position = plantedPosNext;
+            return;
+        }
+        footMoveTimer += Time.deltaTime;
+        float p = footMoveTimer / timeToMoveFoot;
+        p = Mathf.Clamp(p, 0, 1);
+        Vector3 pos = Vector3.Lerp(plantedPosLast, plantedPosNext, footlateralEase.Evaluate(p));
+        pos.y += footVerticalRaise .Evaluate(p);
+        transform.position = pos;
+
+    }
+    void CheckIfCanMoveFoot()
+    {
+        float d2 = (transform.position - homeLocation.position).sqrMagnitude;
+        if (d2 > moveDistanceThreshold * moveDistanceThreshold)
+        {
+            DoRaycast();
+        }
+    }
+    void DoRaycast()
+    {
+        Ray ray = new Ray(homeLocation.position + Vector3.up * 2.5f, Vector3.down);
+        if(Physics.Raycast(ray,out RaycastHit hit, 5))
+        {
+            footMoveTimer = 0;
+            plantedPosLast = transform.position;
+            plantedPosNext = hit.point;
+        }
+    }
+    bool IsAnimating()
+    {
+
+    }
+}
