2d6dc012f2956aa2ef1520fba9378e7129b1feb1
jesse <webbj19@ferris.edu>
2020-04-29 12:18:10 -0400

    made a punch
    added punch to playermovment

--- Assets/scripts/Player/PlayerMovment.cs
+++ Assets/scripts/Player/PlayerMovment.cs
@@ -10,7 +10,8 @@ public class PlayerMovment : MonoBehaviour
     static public float h;
     static public float v;
     public Transform leftFist;
-    public Transform RightFist;
+    public Transform rightFist;
+    public Transform fistTarget;
     bool left;
     bool right = true;
     // Start is called before the first frame update
@@ -22,18 +23,39 @@ public class PlayerMovment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       h = Input.GetAxis("Horizontal");
-       v =Input.GetAxis("Vertical");
-        print(v);
-        print(h);
-        if(v != 0 && theCam != null)
+        Move();
+        MoveToTaregt(rightFist, 1);
+
+        if (Input.GetKey(KeyCode.Mouse0))
         {
-            Quaternion targetRot  = Quaternion.Euler(0, theCam.yaw, 0);
+            MoveToTaregt(rightFist,1);
+        }
+      
+    }
+
+    private void Move()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        
+        if (v != 0 && theCam != null)
+        {
+            Quaternion targetRot = Quaternion.Euler(0, theCam.yaw, 0);
             transform.rotation = AnimMath.Dampen(transform.rotation, targetRot, .01f);
         }
         Vector3 moveDis = transform.forward * v * moveSpeed;
         moveDis += transform.right * h * moveSpeed;
         body.SimpleMove(moveDis);
     }
+    private void MoveToTaregt(Transform fist, int speed)
+    {
 
+        float singleStep = speed * Time.deltaTime;
+
+
+        fist.position = Vector3.MoveTowards(fist.position, fistTarget.position, singleStep);
+    }
 }
+
+
+
