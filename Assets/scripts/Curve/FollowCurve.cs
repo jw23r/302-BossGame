8541c75e85717d59b7c958f3984f5200d4786937
jesse <webbj19@ferris.edu>
2020-05-03 23:53:54 -0400

    made camera cureve

--- Assets/scripts/Curve/FollowCurve.cs
+++ Assets/scripts/Curve/FollowCurve.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class FollowCurve : MonoBehaviour
 {   public BezierCurve curve;
     [Range(0, 1)] public float percent = 0;
-
+    float p;
     public AnimationCurve speed;
     public float animationLength = 5;
     public bool shouldAnimate = true;
     float timeCurrent = 0;
+    float time;
+    public bool shake;
     // Start is called before the first frame update
     void Update()
     {
+        time += Time.deltaTime;
         if (shouldAnimate)
         {
             timeCurrent += Time.deltaTime;
@@ -29,10 +32,40 @@ public class FollowCurve : MonoBehaviour
     {
         if (curve)
         {
-            float p = speed.Evaluate(percent);
-           // print(p);
-            transform.position = curve.FindPositionAt(p);
+            
+            p = speed.Evaluate(percent);
+            // print(p);
+            if (p < 1)
+            {
+                transform.position = curve.FindPositionAt(p);
 
+            }
+            if (p >= 1 && shake)
+            {
+                if (time < .3f)
+                {
+                    transform.localPosition += new Vector3(0, .005f, .005f);
+                }
+                if(time > .4f && time < .8f )
+                {
+                    transform.localPosition += new Vector3(0, 0, -.005f);
+
+                }
+                if (time > .8f && time < 1.2f)
+                {
+                    transform.localPosition += new Vector3(0, -.005f, 0);
+
+                }
+                if (time > 1.2f && time < 1.5f)
+                {
+                    transform.localPosition += new Vector3(0, 0, .005f);
+
+                }
+                if (time >= 1.6f)
+                {
+                    time = 0;
+                }
+            }
         }
     }
 }
